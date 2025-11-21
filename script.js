be13bfa5d0e46247a113468333c23e0f66424996
Rocc <140554598+dummyaccountforforgeforking@users.noreply.github.com>
2025-11-20 20:42:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ function Moon(type) {
 	if (type == "Gold") {
 		var var29 = parseInt(document.getElementById("moonTime").value);
 		var var28 = Math.floor((var29 - (-1800)) / 10800) * 10800;
-		var var27 = new PCGRandom(var28).nextInt(1,100);
+		var var27 = Math.abs(new PCGRandom(var28).nextInt(1,100));
 		while (var27 > 5) {
 			var28 = Math.floor((var29 - (-1800)) / 10800) * 10800;
-			var27 = new PCGRandom(var28).nextInt(1,100);
+			var27 = Math.abs(new PCGRandom(var28).nextInt(1,100));
 			if (var27 <= 5) {
 				console.log(var27);
 				console.log(var29);
@@ -23,14 +23,14 @@ function Moon(type) {
 	if (type == "Blood") {
 		var var29 = parseInt(document.getElementById("moonTime").value);
 		var var28 = Math.floor((var29 - (-1800)) / 10800) * 10800;
-		var var27 = new PCGRandom(var28).nextInt(1,100);
+		var var27 = Math.abs(new PCGRandom(var28).nextInt(1,100));
 		while (var27 > 15) {
 			var28 = Math.floor((var29 - (-1800)) / 10800) * 10800;
-			var27 = new PCGRandom(var28).nextInt(1,100);
+			var27 = Math.abs(new PCGRandom(var28).nextInt(1,100));
 			if (var27 <= 15) {
-				print(var27);
-				print(var29);
-				print(os.date("%a %b %d %I:%M:%S %p %Y", var29));
+				console.log(var27);
+				console.log(var29);
+				console.log(timeStampToDate(var29));
       }
 			var29 = var29 + 10800;
     }
